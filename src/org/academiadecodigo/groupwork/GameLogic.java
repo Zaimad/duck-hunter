611ecf8cfad2187923337283d97bb511e0fbdff4
António Morão <antonio.gdsn.morao@gmail.com>
2020-02-08 18:57:02 +0000

    Game Logic file uploaded for the others.

---
+++ src/org/academiadecodigo/groupwork/GameLogic.java
@@ -0,0 +1,41 @@
+package org.academiadecodigo.groupwork;
+
+public class GameLogic {
+
+    Field field;
+
+    private int delay = 200; //5 actions per second @ 200
+
+    /**
+     * Creates a bunch of cars and randomly puts them in the field
+     */
+    public void init() {
+
+        //field.init();
+
+        //cars = new Car[manufacturedCars];
+        //collisionDetector = new CollisionDetector(cars);
+
+
+
+    }
+
+    /**
+     * Starts the animation
+     *
+     * @throws InterruptedException
+     */
+    public void start() throws InterruptedException {
+
+        while (true) {
+
+            // Pause for a while
+            Thread.sleep(delay);
+
+            //moveAllTargets();
+
+        }
+
+    }
+
+}
