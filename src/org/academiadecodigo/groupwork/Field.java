36c4564ec5d7ef25d36caaf123ef7ef6c5ca39e4
Andre Raja <andre.raja@gmail.comm>
2020-02-08 17:49:40 +0000

    First commit of changes in day 8th Feb @ bootcamp.

--- src/org/academiadecodigo/groupwork/Field.java
+++ src/org/academiadecodigo/groupwork/Field.java
@@ -5,15 +5,36 @@ import org.academiadecodigo.groupwork.pictures.Picture;
 
 public class Field {
 
+    //===============================PROPERTIES===============================//
+
+
     private Picture background;
 
-    public final int PADDING = 10;
+    public final static int PADDING = 10;
 
     private int width;
     private int height;
 
 
+    //===============================CONSTRUCTOR===============================//
 
 
+    public Field(int width, int height, String backgroundImage){
+        background = new Picture(width, height, backgroundImage);
+        this.width = width;
+        this.height = height;
+    }
 
+
+    //===============================METHODS===============================//
+
+
+    //Getters.
+    public int getHeight() {
+        return height + PADDING;
+    }
+
+    public int getWidth() {
+        return width + PADDING;
+    }
 }
