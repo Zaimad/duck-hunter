76c77b09a2ce0ec9c8540cb959efbc8dea75f8e8
António Morão <antonio.gdsn.morao@gmail.com>
2020-02-10 16:00:24 +0000

    Target constructor added for temporary tests regarding hitboxes to be done in the future.

--- src/org/academiadecodigo/groupwork/targets/Target.java
+++ src/org/academiadecodigo/groupwork/targets/Target.java
@@ -2,6 +2,8 @@ package org.academiadecodigo.groupwork.targets;
 
 import org.academiadecodigo.groupwork.Field;
 import org.academiadecodigo.groupwork.FieldSection;
+import org.academiadecodigo.groupwork.graphics.Color;
+import org.academiadecodigo.groupwork.graphics.Rectangle;
 
 public class Target extends FieldSection {
 
@@ -13,15 +15,29 @@ public class Target extends FieldSection {
     //Status.
     private boolean isDead;
 
+    //Temp: Rectangle shapes!
+    Rectangle image;
+    Rectangle hitbox;
+
 //===============================CONSTRUCTOR===============================//
 
 
     public Target(Field field) {
         super(field);
+        image = new Rectangle(getX(),getY(),80,50);
+        image.draw();
+        hitbox = new Rectangle(getX()+10,getY()+10,60,30);
+        hitbox.setColor(Color.DARK_GRAY);
+        hitbox.fill();
     }
 
     public Target(Field field, int x, int y) {
         super(field, x, y);
+        image = new Rectangle(getX(),getY(),80,50);
+        image.draw();
+        hitbox = new Rectangle(getX()+10,getY()+10,60,30);
+        hitbox.setColor(Color.DARK_GRAY);
+        hitbox.fill();
     }
 
 //===============================METHODS===============================//
