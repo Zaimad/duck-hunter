94669dedf503791508317513ad79dc3e825d0466
Joel Ginga <jjginga@gmail.com>
2020-02-10 18:06:42 +0000

    testing

--- src/org/academiadecodigo/groupwork/targets/TargetFactory.java
+++ src/org/academiadecodigo/groupwork/targets/TargetFactory.java
@@ -3,6 +3,7 @@ package org.academiadecodigo.groupwork.targets;
 public class TargetFactory {
 
     public static void generate() {
+        //teste
         System.out.println("THIS METHOD IS NOT DONE YET!");
     }
 
